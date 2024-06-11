a6750f6b7f5e5f1e9a0d067dfebb312bbdca14aa
Snydro <82437065+Snydro@users.noreply.github.com>
2024-06-11 18:27:39 -0500

    Upload v1.0

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'SH-Development'
+description 'Circle Progress Bar'
+version '1.0.0'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+}
